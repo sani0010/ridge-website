b412594a8a0c21cfbcc035909150251c205cf245
sani0010 <sanitamrakar001@gmail.com>
2024-11-10 12:51:58 +0545

    .

--- src/data/eventData.js
+++ src/data/eventData.js
@@ -6,7 +6,7 @@ export const eventData = [
   {
     id: 1,
     image: orientationImage,
-    date: "October 2024",
+    date: "05 October 2024",
     title: "Orientation Day (October)",
     description: "Ridge International College recently hosted its highly anticipated Orientation Program, extending a warm welcome to the incoming class and laying the foundation for an enriching academic journey.",
     fullDescription: "Our Orientation Day was filled with exciting activities, campus tours, and informative sessions designed to help new students acclimate to college life. Students had the opportunity to meet their peers, faculty members, and support staff while learning about the various resources available to them throughout their academic journey.",
@@ -23,7 +23,7 @@ export const eventData = [
   {
     id: 2,
     image: intakeImage,
-    date: "03 June 2023",
+    date: "03 December 2024",
     title: "Intake (October)",
     description: "Ridge International College warmly hosted its highly anticipated Orientation Program, extending a warm welcome to the incoming class and laying the foundation for an enriching academic journey.",
     fullDescription: "The October intake marks an exciting new chapter for our college community as we welcome a diverse group of students from around the world. This intake period demonstrates our commitment to providing quality education and fostering an inclusive learning environment.",
@@ -40,7 +40,7 @@ export const eventData = [
   {
     id: 3,
     image: cupDayImage,
-    date: "01 June 2023",
+    date: "09 December 2024",
     title: "Melbourne Cup Day (November)",
     description: "The Badminton World Federation is developing its services within the country's governing body, Philippine Badminton Association.",
     fullDescription: "Join us for an exciting celebration of the Melbourne Cup Day! This event brings together our college community for a day of festivities, fashion, and fun. Experience the race that stops a nation while enjoying special activities and refreshments.",
