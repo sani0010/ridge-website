6f4c44c76714144e94391820bdc2b26060bd4284
sani0010 <sanitamrakar001@gmail.com>
2024-11-10 09:45:57 +0545

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -166,6 +166,22 @@ const Header = () => {
                 }
               `}
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className={`w-5 h-5 mr-2 transition-transform duration-300 ${
+                  isLoginHovered ? 'rotate-12' : ''
+                }`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                />
+              </svg>
               <span className="font-semibold text-sm">Student Login</span>
             </a>
             <div className={`
